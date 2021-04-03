1d93e64dd4a431f9d53588a11cb76d727184ab94
Bianca-Fernandes <79364731+Bianca-Fernandes@users.noreply.github.com>
2021-04-03 16:49:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject, paperObject, room;	
+var world;
+
+function preload(){
+	room = loadImage("room2.jpg");
+}
+function setup() {
+	createCanvas(1600, 700);//1270, 640
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	paperObject = new Paper(200, 10, 70);
+
+	Engine.run(engine);
+}
+
+function draw() {
+  rectMode(CENTER);
+  background(room);
+ 
+  groundObject.display();
+  paperObject.display();
+  dustbinObj.display();
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(paperObject.body, paperObject.body.position,{x:130,y:-145});
+	}
+}
